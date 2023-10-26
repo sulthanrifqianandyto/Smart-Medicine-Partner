7e397a1e76652c3b7d545fe7fe98b6f0009463f2
sulthanrifqianandyto <sulthanrifqi014@gmail.com>
2023-10-26 13:03:03 +0700

    komit awal

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/index', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about',[
+        "nama"=>"sulthan"
+    ]);
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
